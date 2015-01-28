fedab3d5a2e9237fb557551682bb22da37f03872
deathgodsato <kekigenkai@live.ca>
2015-01-28 16:48:01 -0500

    has working menu and credits i think

---
+++ Labrynith Porject/Assets/scripts/UIManagerScriptJS.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+ function StartGame()
+{
+	Application.LoadLevel("Level1");
+}
+
+function MainMenu()
+{
+	Application.LoadLevel("SplashScreen");
+}
+
+function Credits()
+{
+	Application.LoadLevel("Credits");
+}
+
+function Quit()
+{
+	Application.Quit();
+}	
