7f8208f06a530c90e26d3703de760ff998bc4fab
deathgodsato <kekigenkai@live.ca>
2015-01-29 15:42:17 -0500

    added link between main menu to level 1 to level 2
    also fixed issue in which the main menu stoped working

---
+++ Labrynith Porject/Assets/scripts/LoadNextLevel.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+ 
+function Start () 
+{
+
+}
+
+function Update () 
+{
+	//if()
+	//{
+	//	LoadNextLeve();
+	//}
+}
+
+function OnTriggerEnter(otherObj: Collider)
+{
+	Debug.Log("Collided");
+	Debug.Log(Application.loadedLevelName);
+	LoadNextLevel();
+}
+
+function LoadNextLevel()
+{
+	if(Application.loadedLevelName == "Level2")
+	{
+		Application.LoadLevel("Level1");
+	}
+}
